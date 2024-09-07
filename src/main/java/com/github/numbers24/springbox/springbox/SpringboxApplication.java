2e7c3035a71b37d3a5a613b465059e0105aed0c5
Christopher DeAngelis <deangelis.chris98@gmail.com>
2024-09-07 07:45:59 -0400

    initial commit

---
+++ src/main/java/com/github/numbers24/springbox/springbox/SpringboxApplication.java
@@ -0,0 +1,13 @@
+package com.github.numbers24.springbox.springbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/numbers24/springbox/springbox/SpringboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.numbers24.springbox.springbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
