3f9f671ed36b77519fb6c40e12ce92a94b8f6323
Christopher DeAngelis <deangelis.chris98@gmail.com>
2024-09-07 22:18:49 -0400

    add anagrams get request

---
+++ src/main/java/com/github/numbers24/springbox/util/Anagrams.java
@@ -0,0 +1,45 @@
+package com.github.numbers24.springbox.util;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 
+ */
+public class Anagrams {
+
+    /**
+     * @param s
+     * @return string sorted by its chars
+     */
+    public static String sortString(String s) {
+        return Stream.of(s.split(""))
+                .sorted()
+                .collect(Collectors.joining());
+    }
+
+    /**
+     * 
+     * @param word
+     * @return List of anagrams
+     * @throws URISyntaxException
+     * @throws IOException
+     */
+    public static List<String> searchAnagrams(String word) throws URISyntaxException, IOException {
+
+        String sortedWord = sortString(word);
+
+        URI uri = new URI("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt");
+        try (Scanner scan = new Scanner(uri.toURL().openStream())) {
+            return scan.tokens()
+                    .parallel()
+                    .filter(dw -> sortString(dw).equalsIgnoreCase(sortedWord))
+                    .collect(Collectors.toList());
+        }
+    }
+}
---
+++ src/test/java/com/github/numbers24/springbox/util/AnagramsTest.java
@@ -0,0 +1,25 @@
+package com.github.numbers24.springbox.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class AnagramsTest {
+
+    @Test
+    public void sortString() {
+        assertEquals("eht", Anagrams.sortString("the"));
+    }
+
+    @Test
+    public void searchAngagrams() throws URISyntaxException, IOException{
+        List<String> anagrams = Anagrams.searchAnagrams("there");
+        assertTrue(anagrams.contains("three"));
+    }
+    
+}
